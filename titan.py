975b638cf3cca781ad454e8a54e80f61c79a3f37
Sandeep Athreyas <Athreyas@users.noreply.github.com>
2021-10-01 00:59:55 -0700

    Updated the titan service with more API endpoints

--- titan.py
+++ titan.py
@@ -22,10 +22,10 @@ def home():
 def list_script_names():
     try:
         connection = sqlite3.connect(local_db)
-        cs = connection.cursor()
+        cursor = connection.cursor()
 
-        list_all_scripts = "select script_name from titan_scripts;"
-        records = cs.execute(list_all_scripts).fetchall()
+        list_all_scripts = "select script_name from titan_repo;"
+        records = cursor.execute(list_all_scripts).fetchall()
 
         data = {
                 'No.of Scripts available': len(records),
@@ -38,101 +38,117 @@ def list_script_names():
         return jsonify("Failed to read data from table")
 
     finally:
-        connection.close()
+        cursor.close()
 
 # Below method will print the script with the given script_name
 @app.route('/scripts/<string:name>/', methods=['GET'])
 def fetch_script(name):
     try:
         connection = sqlite3.connect(local_db)
-        cs = connection.cursor()
+        cursor = connection.cursor()
 
-        get_script_value = "SELECT script FROM titan_scripts WHERE script_name='{0}';".format(name)
-        record = list(cs.execute(get_script_value).fetchall()[0])
+        get_script_value = "SELECT script FROM titan_repo WHERE script_name='{0}';".format(name)
+        records = cursor.execute(get_script_value).fetchall()
 
-        if record:
+        if records:
+            record = list(records[0])
             return jsonify({'Name': name, 'Script': record[0]})
         else:
-            output="Requesting script {}, doesn't exist in the DB".format(name)
+            output="Requesting script '{0}'', doesn't exist in the DB".format(name)
             return jsonify(output)
 
     except sqlite3.Error as error:
         return jsonify("Failed to read data from table")
 
     finally:
-        connection.close()
+        cursor.close()
 
 # Below method will upload the script into DB
-@app.route('/scripts/push/', methods=['GET', 'POST'])
+@app.route('/scripts/post/', methods=['POST', 'GET'])
 def upload_script():
     script = request.args.get('script')
 
-    try:
-        connection = sqlite3.connect(local_db)
-        cs = connection.cursor()
+    if request.method == 'POST':
+        try:
+            connection = sqlite3.connect(local_db)
+            cursor = connection.cursor()
 
-        if request.method == 'POST':
-            script_name = request.form.get('script_name')
-            script = request.form.get('script')
+            script_name = request.json.get('script_name')
+            script_value = request.json.get('script_value')
 
-            update_command = "insert into titan_scripts values(null, '{0}', '{1}');".format(script_name, script)
-            record = connection.execute(update_command)
+            update_command = "insert into titan_repo (script_name, script) values ('{0}', '{1}');".format(script_name, script_value)
+            records = cursor.execute(update_command)
+            connection.commit()
 
-            output = '''
-                      <h1>Script name is: {}</h1>
-                      <h1>Input Script is: {}</h1>'''.format(script_name, script)
-            return output
+            output = {'Name': script_name, 'Script Value': script_value}
+            return jsonify(output)
 
-        else:
-            output = '''
-                      <form method="POST">
-                          <div><label>Script Name: <input type="text" name="script_name"></label></div>
-                          <div><label>Scripts: <input type="text" name="script"></label></div>
-                          <input type="submit" value="Submit">
-                      </form>'''
-            return output
+        except sqlite3.Error as error:
+            return jsonify("Failed to connect to the database and insert into the table")
 
-    except sqlite3.Error as error:
-        return jsonify("Failed to insert records into the table")
+        finally:
+            cursor.close()
 
-    finally:
-        connection.close()
+    else:
+        return jsonify("Invaild method used. Only post method allowed.")
+
 
 # Below method will run the input script
 @app.route('/scripts/run/<string:name>/', methods=['GET'])
 def run_script(name):
     try:
         connection = sqlite3.connect(local_db)
-        cs = connection.cursor()
+        cursor = connection.cursor()
 
-        get_script_value = "SELECT uuid, script FROM titan_scripts WHERE script_name='{0}';".format(name)
-        record = list(cs.execute(get_script_value).fetchall()[0])
+        script_name = name
+        get_script_value = "SELECT uuid, script FROM titan_repo WHERE script_name='{0}';".format(script_name)
+        records = list(connection.execute(get_script_value).fetchall()[0])
 
+        process = subprocess.run(records[1], shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
 
-        if record:
-            process = subprocess.run(record[1], shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-            output = process.stdout
+        if not process.returncode:
+            update_script_output = "insert into titan_status (script_uuid, name, script_status ) values( {0}, '{1}', '{2}');".format(records[0], script_name, 'Completed')
+            cursor.execute(update_script_output)
+            connection.commit()
 
-            update_script_output = "insert into titan_scripts_status values(null, {0}, '{1}', ,'{2}');".format(record[0], name, output)
-            update_output = cs.execute(update_script_output).fetchall()
-
-            return jsonify(output, update_output)
+            return jsonify({"Script Status": "Submitted", "Script Output": process.stdout})
 
         else:
-            output="Requesting script '{}', doesn't exist in the DB".format(name)
+            output = "Requesting script '{}', didn't run successfully".format(name)
             return jsonify(output)
 
     except sqlite3.Error as error:
         return jsonify("Failed to read data from table")
 
     finally:
-        connection.close()
+        cursor.close()
 
 # Below method will get the script execution status
 @app.route('/scripts/status/<string:name>/', methods=['GET'])
 def get_script_status(name):
-    return "Status of the script '{}' execution is as follows".format(name)
+    try:
+        connection = sqlite3.connect(local_db)
+        cursor = connection.cursor()
 
+        script_name = name
+        get_script_value = "SELECT script_uuid, run_timestamp, script_status FROM titan_status WHERE script_name='{0}' ORDER BY run_timestamp DESC LIMIT 1;".format(script_name)
+        records = connection.execute(get_script_value).fetchall()
+
+        if records:
+            record = list(records[0])
+            output = "Latest status of the script '{0}' is: {1}".format(script_name, record[2])
+            return jsonify(output)
+
+        else:
+            output = "Requested script '{0}', has not been executed so far.".format(script_name)
+            return jsonify(output)
+
+    except sqlite3.Error as error:
+        return jsonify("Failed to read data from table")
+
+    finally:
+        cursor.close()
 
 if __name__ == '__main__':
     app.run(debug=True, port='5002')
+    #app.run(port='5002')
